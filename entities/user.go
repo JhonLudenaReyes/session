be1ffdf58f60925985af257559af918ed483741f
JhonLudenaReyes <ludena.jhon.j.6392@outlook.com>
2023-11-15 16:10:08 -0500

    Avances api user

--- entities/user.go
+++ entities/user.go
@@ -5,7 +5,7 @@ type User struct {
 	PersonId int    `json:"personId" gorm:"primaryKey"`
 	RoleId   int    `json:"RoleId" gorm:"primaryKey"`
 	User     string `json:"user" gorm:"size:13; not null"`
-	Password string `json:"password" gorm:"size:8; not null"`
+	Password string `json:"password" gorm:"size:16; not null"`
 	State    string `json:"state" gorm:"size:1; not null; default:'A'"`
 	Person   Person `gorm:"foreingKey:'PersonId'"`
 	Role     Role   `gorm:"foreingKey:'RoleId'"`
